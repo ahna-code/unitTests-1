4b9d072076dba4c0983bcb81cc8f336925345617
Agnieszka Korczak <korczak.a.m@gmail.com>
2019-02-03 16:20:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const formatDate = (timeInSeconds) => {
      const minutes = Math.floor((timeInSeconds % 3600) / 60);
      const seconds = Math.floor(timeInSeconds % 60);
 
-     return `${days !== 0 ? `${days}d` : ''}${days && hours ? ' ' : $
+      return `${days !== 0 ? `${days}d` : ''}${days && hours ? ' ' : ''}${hours !== 0 ? `${hours}h` : ''}${(days || hours) && minutes ? ' ' : ''}${minutes !== 0 ? `${minutes}m` : ''}${(days || hours || minutes) && seconds ? ' ' : ''}${seconds !== 0 || (days === 0 && hours === 0 && minutes === 0) ? `${seconds}s` : ''}`;
 };
 
 module.exports = formatDate;
